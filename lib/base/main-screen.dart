b6dd04ad3cd78ba94051155989a6690ec41f6944
azkifairuz <azkiajmal@gmail.com>
2024-11-10 22:36:41 +0700

    dinamic item on sidebar

--- lib/base/main-screen.dart
+++ lib/base/main-screen.dart
@@ -1,23 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:timbangan_app/base/sidebar.dart';
+import 'package:timbangan_app/screen/configuration.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final appScreens = [
+    const Center(child: Text("Search")),
+    const ConfigPage(),
+
+  ];
+   int selectedIndex = 0;
+  void onSidebarItemSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Row(
-        children: [
-          const Expanded(
-              flex:2,
-              child: Sidebar()
-          ),
-          Expanded(
-              flex: 8,
-              child: Container(color: Colors.green,))
-        ],
-      ))
-    );
+    return  Scaffold(
+        body: SafeArea(child: Row(
+          children: [
+            Expanded(
+                flex:2,
+                child: Sidebar(onItemSelected: onSidebarItemSelected)
+            ),
+            Expanded(
+                flex: 8,
+                child:  appScreens[selectedIndex])
+          ],
+        ))
+    );;
   }
 }
--- lib/base/sidebar.dart
+++ lib/base/sidebar.dart
@@ -1,15 +1,62 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:timbangan_app/constant/constant.dart';
+import 'package:timbangan_app/data/sidebar-data.dart';
 
 class Sidebar extends StatefulWidget {
-  const Sidebar({super.key});
+  final Function(int) onItemSelected;
+  const Sidebar({super.key, required this.onItemSelected});
 
   @override
   State<Sidebar> createState() => _SidebarState();
 }
 
 class _SidebarState extends State<Sidebar> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final data = SidebarData();
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
+      color: primaryColor,
+      child: ListView.builder(
+          itemCount: data.menu.length,
+          itemBuilder: (context, index) => buildMenuEntry(data, index)),
+    );
+  }
+
+  Widget buildMenuEntry(SidebarData data, int index) {
+    final isSelected = selectedIndex == index;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(6.0)),
+          color: isSelected ? selectionColor : Colors.transparent),
+      child: InkWell(
+        onTap: () {setState(() {
+          selectedIndex = index;
+          });
+          widget.onItemSelected(index);
+        },
+
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+              child: Icon(
+                data.menu[index].icon,
+                color: isSelected ? primaryColor : Colors.white,
+              ),
+            ),
+            Text(
+              data.menu[index].title,
+              style: TextStyle(
+                  fontSize: 16,
+                  color: isSelected ? primaryColor: Colors.white,
+                  fontWeight: isSelected ? FontWeight.bold :  FontWeight.normal),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
